371ba6fa1e28df9e9edf438a4f5f0ed0a5fcc096
Marek Lach <marek.lach91@gmail.com>
2019-07-09 13:35:58 +0200

    Updates

---
+++ precache-manifest.a060933d43ba3798a30c412894e47422.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b488d14519067710d0dd482a77221e97",
+    "url": "/React-app/static/media/x3.b488d145.jpg"
+  },
+  {
+    "revision": "c37e208ba7aa2925c5c3acd020b6712f",
+    "url": "/React-app/static/media/x2.c37e208b.jpg"
+  },
+  {
+    "revision": "19261c9eec8db466067b2e4103fd31c5",
+    "url": "/React-app/static/media/x1.19261c9e.jpg"
+  },
+  {
+    "revision": "d704105685e9a50e8fe8",
+    "url": "/React-app/static/js/runtime~main.a7bc867e.js"
+  },
+  {
+    "revision": "a6f24cfffd4e7e7db2df",
+    "url": "/React-app/static/js/main.2d610fec.chunk.js"
+  },
+  {
+    "revision": "c06bbfb1341794155422",
+    "url": "/React-app/static/js/2.1388ea29.chunk.js"
+  },
+  {
+    "revision": "a6f24cfffd4e7e7db2df",
+    "url": "/React-app/static/css/main.6df55a4e.chunk.css"
+  },
+  {
+    "revision": "ce302c7369cee9a1184e6ddb333d3b4b",
+    "url": "/React-app/index.html"
+  }
+];
